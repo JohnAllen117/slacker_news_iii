47974627c7a79ab0b6bf9a8f0fd6131f358d5ba1
John Allen <john_allen3@student.uml.edu>
2014-08-27 15:52:44 -0400

    Added Comments

--- schema.sql
+++ schema.sql
@@ -2,12 +2,13 @@ CREATE TABLE articles (
   id serial PRIMARY KEY,
   title varchar(255) NOT NULL,
   url varchar(255) NOT NULL,
-  description varchar(255) NOT NULL,
+  time timestamp NOT NULL
 );
 
 CREATE TABLE comments (
   id serial PRIMARY KEY,
   body varchar(1000) NOT NULL,
-  username varchar(255) NOT NULL,
-  time datetime NOT NULL,
+  username varchar(255),
+  time timestamp NOT NULL,
+  article_id int NOT NULL
 );
--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 require "pg"
-
+require "pry"
 def db_connection
   begin
     connection = PG.connect(dbname: 'slacker_news')
@@ -10,22 +10,30 @@ def db_connection
     connection.close
   end
 end
-
-def find_articles
-  serialized_articles = redis.lrange("slacker:articles", 0, -1)
-
-  articles = []
-
-  serialized_articles.each do |article|
-    articles << JSON.parse(article, symbolize_names: true)
+def format_url(url)
+  unless url.start_with?(("http://") || ("https://"))
+    url.prepend("http://")
   end
-
-  articles
+  url
 end
 
 get '/' do
-  @submission = find_articles
+  @articles = db_connection do |conn|
+    conn.exec('SELECT * FROM articles;')
+  end
+  @articles
   erb :index
+end
+
+get '/comments/:articleid' do
+  @article_id = params[:articleid]
+  @comments = db_connection do |conn|
+    conn.exec_params('SELECT * FROM comments WHERE article_id = $1;', [@article_id])
+  end
+  @articles = db_connection do |conn|
+    conn.exec_params('SELECT title, url, id FROM articles WHERE id = $1;', [@article_id])
+  end
+  erb :comments
 end
 
 get '/submit' do
@@ -35,7 +43,23 @@ end
 post '/submit' do
   title = params["title_form"]
   url = params["url_form"]
-  description = params["description_form"]
+  url = format_url(url)
+  db_connection do |conn|
+    conn.exec_params('INSERT INTO articles(title,url,time)
+               VALUES ($1,$2,now());',[title,url])
+  end
+  redirect '/'
+end
+
+post '/comments' do
+  comment = params["comment_body"]
+  article_id = params["article_id"].to_i
+
+  db_connection do |conn|
+    conn.exec_params('INSERT INTO comments(article_id,body,time)
+               VALUES ($1,$2,now());',[article_id, comment])
+  end
+
 
   redirect '/'
 end
